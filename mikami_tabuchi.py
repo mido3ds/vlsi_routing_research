a3dbd8be73729f11ef48ba979b1ce12be97d35e0
Mahmoud Adas <mido3ds@gmail.com>
2020-05-14 06:00:45 +0200

    mikami: added lines

--- mikami_tabuchi.py
+++ mikami_tabuchi.py
@@ -8,6 +8,22 @@ import numpy as np
 _print = print
 print = lambda *args, **kwargs: _print(*args, **kwargs, file=sys.stderr)
 
+# cell type
+OBSTACLE = 0
+VIA = 1
+SRC = 2
+DEST = 3
+
+
+def is_cell(v: int, cell_type: int) -> bool:
+    # i.e. is_cell(2, VIA) => True
+    return (v >> cell_type) & 1 == 1
+
+
+def put_cell(v: int, cell_type: int) -> int:
+    # i.e. put_cell(0, SRC) => 4
+    return v | 1 << cell_type
+
 
 class Point(NamedTuple):
     d: int
@@ -35,6 +51,48 @@ class Path(NamedTuple):
         for i in range(len(self.points)-1):
             s += self.points[i].dist(self.points[i+1])
         return s
+
+
+def horizontal_line(grid: np.ndarray, p: Point, cell_type: int) -> (Line, np.ndarray):
+    max_w = p.w
+    min_w = p.w
+
+    for w in range(p.w, grid.shape[2]):
+        if is_cell(grid[p.d, p.h, w], OBSTACLE):
+            break
+
+        max_w = w
+        grid[p.d, p.h, w] = put_cell(grid[p.d, p.h, w], cell_type)
+
+    for w in range(p.w, 0, -1):
+        if is_cell(grid[p.d, p.h, w], OBSTACLE):
+            break
+
+        min_w = w
+        grid[p.d, p.h, w] = put_cell(grid[p.d, p.h, w], cell_type)
+
+    return Line(p._replace(w=min_w), p._replace(w=max_w)), grid
+
+
+def vertical_line(grid: np.ndarray, p: Point, cell_type: int) -> (Line, np.ndarray):
+    max_h = p.h
+    min_h = p.h
+
+    for h in range(p.h, grid.shape[1]):
+        if is_cell(grid[p.d, h, p.w], OBSTACLE):
+            break
+
+        max_h = h
+        grid[p.d, h, p.w] = put_cell(grid[p.d, h, p.w], cell_type)
+
+    for h in range(p.h, 0, -1):
+        if is_cell(grid[p.d, h, p.w], OBSTACLE):
+            break
+
+        min_h = h
+        grid[p.d, h, p.w] = put_cell(grid[p.d, h, p.w], cell_type)
+
+    return Line(p._replace(h=min_h), p._replace(h=max_h)), grid
 
 
 def solve(grid, src_coor, dest_coor) -> List[Path]:
