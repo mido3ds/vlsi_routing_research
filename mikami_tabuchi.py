75cb9fb93eb97643e8a1ee0baf5f2a7640615408
Mahmoud Adas <mido3ds@gmail.com>
2020-05-16 07:53:03 +0200

    mikami: added backtracking

--- mikami_tabuchi.py
+++ mikami_tabuchi.py
@@ -1,7 +1,9 @@
 #!/bin/env python3
+from __future__ import annotations
+
 import json
 import sys
-from typing import NamedTuple, List, Generator, Union
+from typing import Generator, List, NamedTuple, Union
 
 import numpy as np
 
@@ -34,7 +36,7 @@ class Point(NamedTuple):
     h: int
     w: int
 
-    def dist(self, b) -> int:
+    def dist(self, b: Point) -> int:
         # ignores the `d`
         return abs(self.h - b.h) + abs(self.w - b.w)
 
@@ -43,7 +45,7 @@ class Point(NamedTuple):
         l[i] = value
         return Point._make(l)
 
-    def __eq__(self, a) -> bool:
+    def __eq__(self, a: Point) -> bool:
         for i in range(3):
             if self[i] != a[i]:
                 return False
@@ -53,6 +55,9 @@ class Point(NamedTuple):
 class Line(NamedTuple):
     a: Point
     b: Point
+
+    # parent of this line
+    p: Union[Point, Line]
 
     def is_vertical(self) -> bool:
         return self.a.d == self.b.d and self.a.w == self.b.w
@@ -73,7 +78,7 @@ class Line(NamedTuple):
         for x in range(a, b, step):
             yield self.a._replace_i(dim, x)
 
-    def intersection(self, l2) -> Point:
+    def intersection(self, l2: Line) -> Point:
         sv, l2v = self.is_vertical(), l2.is_vertical()
         assert (sv and not l2v) or (not sv and l2v),\
             f'lines {self}, {l2} must be one vertical and the other horizontal, not both'
@@ -85,19 +90,43 @@ class Line(NamedTuple):
 
         return Point(d=l2.a.d, w=x.a.w, h=y.a.h)
 
-    def __contains__(self, c):
+    def __contains__(self, c: Point):
         return c.d == self.a.d and \
             ((c.h == self.a.h == self.b.h) or (c.w == self.a.w == self.b.w))
 
+    def backtrack(self) -> (Point, List[Line]):
+        lines = [self]
+        parent = self.p
+        assert parent is not None
 
-class Path(NamedTuple):
-    points: List[Point]
+        while type(parent) != Point:
+            lines.append(parent)
+            parent = parent.p
 
-    def len(self) -> int:
-        s = 0
-        for i in range(len(self.points)-1):
-            s += self.points[i].dist(self.points[i+1])
-        return s
+            assert parent is not None
+
+        return parent, lines
+
+
+Path = List[Point]
+
+
+def intersect_lines(lines: List[Line]) -> Path:
+    path: Path = []
+    for i in range(len(lines)-1):
+        path.append(lines[i].intersection(lines[i+1]))
+    return path
+
+
+def build_path(a: Line, b: Line) -> Path:
+    # src --> a . b --> dst
+
+    src, a_to_src = a.backtrack()
+    src_to_a = list(reversed(a_to_src))
+
+    dst, b_to_dst = b.backtrack()
+
+    return [src] + intersect_lines(src_to_a + b_to_dst) + [dst]
 
 
 def add_lines(grid: np.ndarray, p0: Point, cell_type: int, dim: int, enable_recursion=True) -> List[Line]:
@@ -140,8 +169,8 @@ def solve_one_target(grid: np.ndarray, src_coor: Point, dest_coor: Point, src_le
 
     # # start with vert+hor lines for target
     # # each line has T as parent backtracking point
-    # levels[1].append(horizontal_line(...))
-    # levels[1].append(vertical_line(...))
+    # levels[1].append(add_lines(grid, dest_coor, DEST, 1))
+    # levels[1].append(add_lines(grid, dest_coor, DEST, 2))
 
     # # while no new craeted lines for both S and T:
     # while len(levels[0][-1]) != 0 and len(levels[0][-1]) != 0
@@ -162,16 +191,18 @@ def solve_one_target(grid: np.ndarray, src_coor: Point, dest_coor: Point, src_le
     #                   # clean grid of dest
     #                   return path
     # # clean grid of dest
-    # return Path([])
+    # return []
 
     # TODO
-    return Path([Point(5, 6, 7), Point(1, 2, 3)])
+    return [Point(5, 6, 7), Point(1, 2, 3)]
 
 
 def solve(grid: np.ndarray, src_coor: Point, dest_coor: List[Point]) -> List[Path]:
     # TODO
     # start with vert+hor lines for src
     # each line has S as parent backtracking point
+    # levels[1].append(add_lines(grid, src_coor, SRC, 1))
+    # levels[1].append(add_lines(grid, src_coor, SRC, 2))
 
     src_levels: List[List[Line]] = []
 
@@ -197,8 +228,8 @@ if __name__ == "__main__":
     }
 
     for path in paths:
-        out['path_exists'].append(len(path.points) != 0)
-        out['path_length'].append(path.len())
-        out['path_coor'].append([list(x) for x in path.points])
+        out['path_exists'].append(len(path) != 0)
+        out['path_length'].append(len(path))
+        out['path_coor'].append([list(x) for x in path])
 
     json.dump(out, sys.stdout)
