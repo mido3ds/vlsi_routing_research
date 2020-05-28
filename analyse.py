6f06117e2339a4e3dcf66675a644befa86de2961
Mahmoud Adas <mido3ds@gmail.com>
2020-05-28 10:44:27 +0200

    analyse

--- analyse.py
+++ analyse.py
@@ -7,31 +7,61 @@ import json
 import sys
 import math
 
-# from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib import cm
-from matplotlib.ticker import FormatStrFormatter, LinearLocator
 
 inp = json.load(sys.stdin)
 
-for algo, color in (('mikami_tabuchi.py', '.'), ('mod_a_star.py', '^'), ('steiner_tree.py', '.')):#, ('maze_lee.py', 'o')):
+algos = {}
+for algo, color in (('mikami_tabuchi.py', '.'), ('mod_a_star.py', '.'), ('steiner_tree.py', '.'), ('maze_lee.py', '.')):
     subset = list(filter(lambda x: x['algo'] == algo, inp))
-    areas = np.array(list(map(lambda x: x['w'], subset)))
-    ns = np.array(list(map(lambda x: x['n'], subset)))
-    times = np.array(
-        list(map(lambda x: x['time'] if type(x['time']) != str else math.inf, subset)))
-    ds = np.array(list(map(lambda x: x['d'], subset)))
 
+    algos[algo] = {
+        'areas': np.array(list(map(lambda x: x['w'], subset))),
+        'ns': np.array(list(map(lambda x: x['n'], subset))),
+        'times': np.array(
+            list(
+                map(lambda x: x['time'] if type(x['time']) != str
+                    else math.inf, subset)
+            )
+        ),
+        'costs': np.array(list(map(lambda x: x['total_cost'], subset))),
+        'targets': np.array(list(map(lambda x: x['total_targets'], subset))),
+        'color': color
+    }
+
+plt.clf()
+plt.title('W=H=45, different N (num of targets)')
+for label, algo in algos.items():
+    cond = algo['areas'] == 45
     plt.plot(
-        ns, times,
-        color,
-        label=algo
+        algo['ns'][cond], algo['times'][cond],
+        algo['color'],
+        label=label
     )
-    # plt.xticks(list(set(ns)))
+    plt.xticks(list(set(algo['ns'][cond])))
 
-    plt.xlabel('Width and Height')
-    plt.ylabel('Running Time (seconds)')
-
+plt.xlabel('Number of Targets')
+plt.ylabel('Running Time (seconds)')
 plt.legend()
-plt.show()
+plt.savefig('areaConst.png')
+
+plt.clf()
+plt.title('N=5, different W,H (where W=H)')
+for label, algo in algos.items():
+    cond = algo['areas'] != 45
+    plt.plot(
+        algo['areas'][cond], algo['times'][cond],
+        algo['color'],
+        label=label
+    )
+    plt.xticks(list(set(algo['areas'][cond])))
+
+plt.xlabel('W or H')
+plt.ylabel('Running Time (seconds)')
+plt.legend()
+plt.savefig('nConst.png')
+
+
+def sort_x(x, y):
+    return zip(*sorted(zip(x, y), key=lambda x: x[0]))
